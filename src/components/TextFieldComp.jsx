dc1ac6bebd4f156a57a29e096a2015c192f1e8e2
rngnnn <rngnnnclk@gmail.com>
2024-08-26 12:21:06 +0200

    use MUI

--- src/components/TextFieldComp.jsx
+++ src/components/TextFieldComp.jsx
@@ -1,9 +1,130 @@
-import React from 'react'
+import {
+  
+  Avatar,
+  Box,
+  Button,
+  Container,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  TextField,
+  Typography,
+ 
+} from "@mui/material";
+import React, { useState } from "react";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 const TextFieldComp = () => {
-  return (
-    <div>TextFieldComp</div>
-  )
-}
+  let errorr = false;
 
-export default TextFieldComp
+const[show,setShow]=useState(false)
+
+  const currencies = [
+    {
+      value: "USD",
+      label: "$",
+    },
+    {
+      value: "EUR",
+      label: "€",
+    },
+    {
+      value: "BTC",
+      label: "฿",
+    },
+    {
+      value: "JPY",
+      label: "¥",
+    },
+  ];
+
+  return (
+    <div>
+      <Container sx={{ marginTop: 4 }}>
+        <Typography variant="h4">TEXT-FIELD (input)</Typography>
+
+        <Box sx={{ mt: 4 }}>
+          {/* margin="dense"=  az bir margin verir */}
+          <TextField
+            required
+            id="outlined-required"
+            label="Required"
+            defaultValue="email giriniz"
+            margin="dense"
+          />
+
+          <TextField
+            required
+            id="filled-required"
+            label="Filled"
+            defaultValue="Hello World"
+            variant="filled"
+            margin="normal"
+          />
+
+          <TextField
+            error={errorr}
+            id="filled-error-helper-text"
+            label="Error"
+            defaultValue="Hello World"
+            helperText={errorr && "yanlış girdiniz"}
+            variant="filled"
+            fullWidth
+          />
+
+          <TextField
+            id="outlined-select-currency"
+            select
+            label="Select"
+            defaultValue="EUR"
+            helperText="Please select your currency"
+            margin="normal"
+          >
+            {/* https://mui.com/material-ui/react-select/ */}
+            {currencies.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          {/* text-fields icon */}
+
+          <FormControl sx={{ m: 2, width: "25ch" }} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">
+              Password
+            </InputLabel>
+            <OutlinedInput
+              type={show ? "text" : "password"}
+              id="outlined-adornment-password"
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    edge="end"
+                    onClick={() => setShow(!show)}
+                  >
+                    {show ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+
+          <Button fullWidth variant="contained" color="error" sx={{background:"blue",gap:3}}>
+            SUBMIT
+            <Avatar src="https://cdn.pixabay.com/photo/2019/11/29/21/30/girl-4662158_1280.jpg" />
+          </Button>
+        </Box>
+      </Container>
+    </div>
+  );
+};
+
+export default TextFieldComp;
--- src/components/TypoButtons.jsx
+++ src/components/TypoButtons.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
-
+import {Box, Button, Container, Typography} from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import DeleteIcon from "@mui/icons-material/Delete";
 const TypoButtons = () => {
   return (
-    <div>TypoButtons</div>
-  )
+    <div>
+      <Container maxWidth="md">
+        {/* maxWidth="md" width en fazla md büyüklükte olsun.  gutterBottom={true}
+      alttan margin. component attribute ü  sadece console da etkilidir , görüntü yine variant neyse odur*/}
+
+        <Typography
+          variant="h1"
+          component="h3"
+          gutterBottom
+          align="center"
+          color="error"
+          sx={{ background: "#eee", mt: 4, border: "1px solid red" }}
+        >
+          Typography Container Box Button
+        </Typography>
+
+        {/* body1: p etiketidir,body1=16px daha büyük fs ,body2=14px daha küçük font lu.  button=span etiketidir+büyük harf */}
+        <Typography variant="button"> span olacak Lorem, ipsum.</Typography>
+
+        <Typography variant="body1">
+          p etiketi Lorem ipsum dolor sit amet. Lorem, ipsum dolor.
+        </Typography>
+        <Typography variant="body2">
+          p etiketi Lorem ipsum dolor sit amet. Lorem, ipsum dolor.
+        </Typography>
+      </Container>
+
+      <Box
+        sx={{ display: "flex", flexDirection: "column", marginTop: 4, gap: 2 }}
+      >
+        <Button variant="outlined" color="error" startIcon={<DeleteIcon />}>
+          DELETE
+        </Button>
+
+        <Button
+          variant="contained"
+          color="success"
+          sx={{ color: "blue" }}
+          endIcon={<SendIcon />}
+        >
+          SEND
+        </Button>
+      </Box>
+    </div>
+  );
 }
 
 export default TypoButtons
