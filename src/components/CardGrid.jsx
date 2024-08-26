c7e1847ad4846e02baff35d01ee20aba2f3d154e
rngnnn <rngnnnclk@gmail.com>
2024-08-26 12:39:33 +0200

    create navbar

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { Card, CardActions, CardContent, CardMedia, Container, Grid, Typography,Button } from '@mui/material'
+
+import React, { useEffect, useState } from 'react'
 
 const CardGrid = () => {
-  return (
-    <div>CardGrid</div>
+
+const[products,setProducts]=useState([])
+
+useEffect(()=>{
+  fetch("https://dummyjson.com/products").then((res)=>res.json()).then((data)=>setProducts(data.products)
   )
+},[])
+
+
+
+
+
+  return (
+    <Container>
+      <Typography variant="h4" align="center" color="error">
+        {" "}
+        CARD-GRID
+      </Typography>
+      {/* spacing={2}=card lar arası boşluk */}
+      <Grid container spacing={2}>
+        {products.map((a) => (
+          <Grid item key={a.id}  xs={6} md={4}  >
+            <Card sx={{ maxWidth: 345, height:450 }}>
+              <CardMedia
+                sx={{ height: 250 }}
+                image={a.images}
+                title="green iguana"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {a.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {a.description}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button variant="contained" size="small">
+                  Share
+                </Button>
+                <Button variant="contained" color="error" size="small">
+                  Learn More
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 }
 
 export default CardGrid
